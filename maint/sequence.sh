6bbcda5ffe17c3f5913442ab21389815987d98cb
Michael Stone <mistone@akamai.com>
2015-02-07 13:54:25 -0500

    Pass through sequence.sh options.

--- maint/sequence.sh
+++ maint/sequence.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 (cd sequence; go build)
-./sequence/sequence -o=focus.err.tex
+./sequence/sequence -o=focus.err.tex "$@"
 pdflatex focus.err.tex
 xdg-open focus.err.pdf || open focus.err.pdf
