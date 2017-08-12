1b447b8d37eaacda416b96d9a36fa15f9ae6c252
Michael Stone <mistone@akamai.com>
2017-08-12 16:01:10 -0400

    nix: refresh dependencies

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 # NOTE: for information on how to manage dependencies, please see docs/HACKING.adoc
 
-NIXPKGS_URL ?= https://github.com/NixOS/nixpkgs/archive/69420c52423f603083f5136430bfaa501c8b1c65.tar.gz
-NIXPKGS_HASH ?= 0c2fdq1mzzbj0avgi31g5kb3rpzslaymnybwqbnys31ja0nz5lij
-NIXPKGS_STOREPATH := $(shell env PRINT_PATH=1 nix-prefetch-url $(NIXPKGS_URL) $(NIXPKGS_HASH) 2>/dev/null | (read IGNORE; read STORE_PATH; echo $$STORE_PATH))
+NCPU ?= $(shell nproc || sysctl -n hw.ncpu)
+NCPU := $(NCPU)
 
 all:
-	nix-build -j4 -I nixpkgs=file://$(NIXPKGS_STOREPATH) -A bin shell.nix
+	nix-build --cores $(NCPU) -j$(NCPU) -A bin shell.nix
 
 dev:
-	nix-shell -j4 -I nixpkgs=file://$(NIXPKGS_STOREPATH)
+	nix-shell --cores $(NCPU) -j$(NCPU) -A bin
 
 docker:
-	nix-build -j4 -I nixpkgs=file://$(NIXPKGS_STOREPATH) docker.nix
+	nix-build --cores $(NCPU) -j$(NCPU) -A bin docker.nix
 
 .PHONY: all dev docker
 .DEFAULT_GOAL := all
--- default.nix
+++ default.nix
@@ -1,62 +1,35 @@
-{stdenv, lib, goPackages, git, runCommand}:
-let
-	removeReferences = [ goPackages.go ];
-	removeExpr = refs: lib.flip lib.concatMapStrings refs (ref: ''
-		| sed "s,${ref},$(echo "${ref}" | sed "s,$NIX_STORE/[^-]*,$NIX_STORE/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee,"),g" \
-	'');
-in
-goPackages.buildGoPackage rec {
-	name = "focus";
+{stdenv, lib, buildGoPackage, git, runCommand, go2nix, gopherjs, go-bindata, esc}:
+buildGoPackage rec {
+    name = "focus";
+    version = "git${stdenv.lib.strings.substring 0 7 rev}";
+    rev = (builtins.readFile (./.git + ("/" + (builtins.replaceStrings ["\n"] [""] (stdenv.lib.strings.removePrefix "ref: " (builtins.readFile ./.git/HEAD))))));
 
-	goPackagePath = "github.com/mstone/focus";
+    goPackagePath = "github.com/mstone/focus";
+    goDeps = ./godeps.nix;
 
-	src = runCommand "focus-git" {
-		nativeBuildInputs = [ git ];
-		preferLocalBuild = true;
-		localRev = builtins.readFile ./.git/refs/heads/master;
-	} ''
-		mkdir -p $out && git -C ${toString ./.} archive --format=tar $localRev | tar xf - -C $out
-		git -C ${toString ./.} submodule foreach 'git -C ${toString ./.}/$path archive HEAD | tar xf - -C $out/$path'
-		cp -r ${toString ./.}/.git $out/.git
-	'';
+    buildInputs = [ go2nix gopherjs gopherjs.bin go-bindata.bin esc.bin ];
 
-	buildInputs = [
-	] ++ (with goPackages; [
-		log15
-		negroni
-		websocket
-		go-sqlite3
-		render
-		logfmt
-		go-bindata-html-template
-		sqlx
-		juju-errors
-		gopherjs
-		gopherjs.bin
-		go-bindata.bin
-		esc.bin
-		goconvey.bin
-	]);
+    src = runCommand "focus-git" {
+        nativeBuildInputs = [ git ];
+        preferLocalBuild = true;
+        localRev = rev;
+    } ''
+        mkdir -p $out && git -C ${toString ./.} archive --format=tar $localRev | tar xf - -C $out
+        git -C ${toString ./.} submodule foreach 'git -C ${toString ./.}/$path archive HEAD | tar xf - -C $out/$path'
+        cp -r ${toString ./.}/.git $out/.git
+    '';
 
-	postConfigure = ''
-		(cd go/src/${goPackagePath}; go generate)
-	'';
+    postConfigure = ''
+        (export GOPATH="`pwd`/go:$GOPATH"; cd go/src/${goPackagePath}; go generate)
+    '';
 
-	excludedPackages = "kitchen-sink";
+    excludedPackages = "kitchen-sink";
 
-	preFixup = ''
-		while read file; do
-			cat $file ${removeExpr removeReferences} > $file.tmp
-			mv $file.tmp $file
-			chmod +x $file
-		done < <(find $out -type f 2>/dev/null)
-	'';
-
-	shellHook = ''
-		if [ -z "${builtins.getEnv "GOPATH"}" ]; then
-			echo "WARNING: the focus dev-shell requires that the focus source code working copy be placed or mounted in a standard GOPATH workspace, not accessed via symlink"
-			echo "ERROR: GOPATH not set; exiting"
-			exit 1
-		fi
-	'';
+    shellHook = ''
+        if [ -z "${builtins.getEnv "GOPATH"}" ]; then
+            echo "WARNING: the focus dev-shell requires that the focus source code working copy be placed or mounted in a standard GOPATH workspace, not accessed via symlink"
+            echo "ERROR: GOPATH not set; exiting"
+            exit 1
+        fi
+    '';
 }
